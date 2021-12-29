e00b681959348d226c48b632f10a9eb4854ba4b7
sukhbirsekhon <sekhonss@mail.uc.edu>
2021-12-28 22:35:16 -0500

    GET All products endpoint

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,7 +3,13 @@ const router = express();
 
 const Product = require('../database/models/product');
 
- router.get('/inventories/:inventoryId/products', (req, res) => {
+router.get('/products', (req, res) => {
+    Product.find({})
+    .then(products => res.send(products))
+    .catch((error) => console.log(error));
+});
+
+router.get('/inventories/:inventoryId/products', (req, res) => {
     Product.find({ _inventoryId: req.params.inventoryId })
     .then((products) => res.send(products))
     .catch((error) => console.log(error));
