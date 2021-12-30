f137e1716aad276faea9ea2f5be0d07e74103533
sukhbirsekhon <sekhonss@mail.uc.edu>
2021-12-30 17:55:18 -0500

    Update Inventory and Product endpoint added

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -3,12 +3,14 @@ const router = express();
 
 const Inventory = require('../database/models/inventory');
 
+// GET All Inventories
 router.get('/inventories', (req, res) => {
     Inventory.find({})
     .then(inventories => res.send(inventories))
     .catch((error) => console.log(error));
 });
 
+// ADD Inventory
 router.post('/inventories', (req, res) => {
     (new Inventory({
         'inventoryName': req.body.inventoryName, 
@@ -18,10 +20,23 @@ router.post('/inventories', (req, res) => {
         .catch((error) => console.log(error));    
 });
 
+// GET Inventory By ID
 router.get('/inventories/:inventoryId', (req, res) => {
     Inventory.find({ _id: req.params.inventoryId })
     .then((inventory) => res.send(inventory))
     .catch((error) => console.log(error));
+});
+
+// Update Inventory
+router.patch('/inventories/:inventoryId', async (req, res) => {
+    try {
+        const updatedInventory = await Inventory.findOneAndUpdate({ _inventoryId: req.params.inventoryId }, req.body, {
+            new: true
+        } )
+        res.send(updatedInventory);
+    } catch(e) {
+        res.status(404).send(updatedInventory)
+    }
 });
 
 module.exports = router;
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,18 +3,21 @@ const router = express();
 
 const Product = require('../database/models/product');
 
+// GET All Products
 router.get('/products', (req, res) => {
     Product.find({})
     .then(products => res.send(products))
     .catch((error) => console.log(error));
 });
 
+//GET Products By Inventory ID
 router.get('/inventories/:inventoryId/products', (req, res) => {
     Product.find({ _inventoryId: req.params.inventoryId })
     .then((products) => res.send(products))
     .catch((error) => console.log(error));
 });
 
+// ADD Product
 router.post('/inventories/:inventoryId/products', (req, res) => {
     (new Product({
         'productName': req.body.productName, 
@@ -30,9 +33,28 @@ router.post('/inventories/:inventoryId/products', (req, res) => {
         .catch((error) => console.log(error)); 
 });
 
+// GET Product By Product ID
 router.get('/inventories/:inventoryId/products/:productId', (req, res) => {
     Product.find({ _id: req.params.productId })
     .then((product) => res.send(product))
+    .catch((error) => console.log(error));
+});
+
+// Update Product
+router.patch('/inventories/:inventoryId/products/:productId', async (req, res) => {
+    try {
+        const updatedProduct = await Product.findOneAndUpdate({ _inventoryId: req.params.inventoryId, _id: req.params.productId }, req.body, {
+            new: true
+        } )
+        res.send(updatedProduct);
+    } catch(e) {
+        res.status(404).send(updatedProduct)
+    }
+});
+
+router.delete('/inventories/:inventoryId/products/:productId', (req, res) => {
+    Product.findOneAndDelete({ _inventoryId: req.params.inventoryId, _id: req.params.productId })
+    .then((products) => res.send(products))
     .catch((error) => console.log(error));
 });
 
